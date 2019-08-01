4237b2a6503b0d12d6fdf1643e98e0fa05e33537
qiaosiyi <qsy2011815@163.com>
2019-08-01 12:13:01 +0800

    Update tcptestclient.c

--- tcptestclient.c
+++ tcptestclient.c
@@ -22,3 +22,27 @@ int main(int argc, char *argv[]){
    }
    MPI_Finalize();
 }
+
+
+#include "mpi.h"
+int main( int argc, char **argv )
+{
+   MPI_Comm server;
+   double buf[MAX_DATA];
+   char port_name[MPI_MAX_PORT_NAME];
+   MPI_Init( &argc, &argv );
+   strcpy(port_name, argv[1] );/* assume server’s name is cmd-line arg */
+   MPI_Comm_connect( port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD,
+   &server );
+   while (!done) {
+      tag = 2; /* Action to perform */
+      MPI_Send( buf, n, MPI_DOUBLE, 0, tag, server );
+      /* etc */
+   }
+   MPI_Send( buf, 0, MPI_DOUBLE, 0, 1, server );
+   MPI_Comm_disconnect( &server );
+   MPI_Finalize();
+   return 0;
+}
+
+
