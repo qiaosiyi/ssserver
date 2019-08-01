18b611567fd4ade52c4da0a46ea65b02a8539418
qiaosiyi <qsy2011815@163.com>
2019-08-01 12:12:19 +0800

    Update tcptestserver.c

--- tcptestserver.c
+++ tcptestserver.c
@@ -24,3 +24,40 @@ int main(int argc, char *argv[]){
    MPI_Finalize();
 }
                           
+#include "mpi.h"
+int main( int argc, char **argv )
+{
+   MPI_Comm client;
+   MPI_Status status;
+   char port_name[MPI_MAX_PORT_NAME];
+   double buf[MAX_DATA];
+   int size, again;
+   MPI_Init( &argc, &argv );
+   MPI_Comm_size(MPI_COMM_WORLD, &size);
+   if (size != 1) error(FATAL, "Server too big");
+   MPI_Open_port(MPI_INFO_NULL, port_name);
+   printf("server available at %s\n",port_name);
+   while (1) {
+      MPI_Comm_accept( port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD,
+      &client );
+      again = 1;
+      while (again) {
+         MPI_Recv( buf, MAX_DATA, MPI_DOUBLE,
+         MPI_ANY_SOURCE, MPI_ANY_TAG, client, &status );
+         switch (status.MPI_TAG) {
+            case 0: MPI_Comm_free( &client );
+            MPI_Close_port(port_name);
+            MPI_Finalize();
+            return 0;
+            case 1: MPI_Comm_disconnect( &client );
+            again = 0;
+            break;
+            case 2: /* do something */
+            ...
+            default:
+            /* Unexpected message type */
+            MPI_Abort( MPI_COMM_WORLD, 1 );
+         }
+      }
+   }
+}
