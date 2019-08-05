88670174504a336c819de6a57ea1324a59b665c2
qiaosiyi <qsy2011815@163.com>
2019-08-05 21:16:51 +0800

    Update tcptestclient.c

--- tcptestclient.c
+++ tcptestclient.c
@@ -4,45 +4,25 @@
 #include<string.h>
 
 int size, rank;
+char * msg_string;
 int main(int argc, char *argv[]){
    MPI_Comm servidor;
    int msg, tag, dest;
    char portname[MPI_MAX_PORT_NAME];
-
+   char* msg_string;
+   msg_string = (char *) malloc(256);
+   strcpy(msg_string, "World!");
    MPI_Init(&argc,&argv);
    MPI_Comm_size(MPI_COMM_WORLD,&size);
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-   if (argc >= 2){
-      printf("Trying connect to %s\n", argv[1]);
-      strcpy(portname, argv[1]);
-      MPI_Comm_connect(portname, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &servidor);
-      msg = 42; tag = 0; dest = 0;
-      MPI_Send(&msg, 1, MPI_INT, dest, tag, servidor);
-      MPI_Comm_disconnect(&servidor);
-   }
+   printf("Trying connect to : \n");
+   gets(portname);
+   MPI_Comm_connect(portname, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &servidor);
+   msg = 42; tag = 0; dest = 0;
+   MPI_Send(msg_string, 7, MPI_CHAR, dest, tag, servidor);
+   printf("client mpi rank = %d, size = %d, dest = %d, tag = %d\n",rank, size, dest, tag);
+   MPI_Comm_disconnect(&servidor);
    MPI_Finalize();
-}
-
-
-#include "mpi.h"
-int main( int argc, char **argv )
-{
-   MPI_Comm server;
-   double buf[MAX_DATA];
-   char port_name[MPI_MAX_PORT_NAME];
-   MPI_Init( &argc, &argv );
-   strcpy(port_name, argv[1] );/* assume server’s name is cmd-line arg */
-   MPI_Comm_connect( port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD,
-   &server );
-   while (!done) {
-      tag = 2; /* Action to perform */
-      MPI_Send( buf, n, MPI_DOUBLE, 0, tag, server );
-      /* etc */
-   }
-   MPI_Send( buf, 0, MPI_DOUBLE, 0, 1, server );
-   MPI_Comm_disconnect( &server );
-   MPI_Finalize();
-   return 0;
 }
 
 
